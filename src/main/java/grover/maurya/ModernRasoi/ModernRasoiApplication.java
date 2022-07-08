ee872ce3574429a20e985b33984798ba7dc1ae54
Maurya-Grover <maurya.grover@gmail.com>
2022-07-08 07:40:40 +0530

    initial commit

---
+++ src/main/java/grover/maurya/ModernRasoi/ModernRasoiApplication.java
@@ -0,0 +1,13 @@
+package grover.maurya.ModernRasoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernRasoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernRasoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grover/maurya/ModernRasoi/ModernRasoiApplicationTests.java
@@ -0,0 +1,13 @@
+package grover.maurya.ModernRasoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernRasoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
